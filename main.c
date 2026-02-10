e336e80dd2e4ff2fc88e8fa83717dc1583be7669
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-10 19:24:27 -0300

    feat: creating basic 24-bit reader

--- main.c
+++ main.c
@@ -31,8 +31,6 @@ int main()
         printf("Is not a Bitmap image\n");
     }
 
-    fseek(bitmap_image, file_header->pixel_offset, SEEK_SET);
-
     if (info_header->bits_per_pixel != 24)
     {
         printf("It's not 24 bit\n");
@@ -43,6 +41,40 @@ int main()
         printf("Compression is not 0\n");
         return 0;
     }
+
+    fseek(bitmap_image, file_header->pixel_offset, SEEK_SET);
+
+    int width = info_header->width;
+    int height = info_header->height;
+
+    int bytes_per_pixel = 3;
+    int row_bytes = width * bytes_per_pixel;
+
+    int padding = (4 - (row_bytes % 4)) % 4;
+    int stride = row_bytes + padding;
+
+    unsigned char *row_buffer = malloc(stride);
+    unsigned char *image = malloc(width * height * 3);
+
+    for (int y = 0; y < height; y++)
+    {
+        fread(row_buffer, 1, stride, bitmap_image);
+
+        int target_y = height - 1 - y;
+
+        for (int x = 0; x < width; x++)
+        {
+            int src = x * 3;
+            int dst = (target_y * width + x) * 3;
+
+            image[dst + 0] = row_buffer[src + 2]; // R
+            image[dst + 1] = row_buffer[src + 1]; // G
+            image[dst + 2] = row_buffer[src + 0]; // B
+        }
+    }
+
+    free(row_buffer);
+
     free(info_header);
     free(file_header);
 }
